250967563f4fb5559f0521a3f035afa3b946f395
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-11-01 08:16:00 -0500

    Update test.js

--- test.js
+++ test.js
@@ -64,8 +64,8 @@ function countChar(str, char) {
     return count;
 }
 
-console.log(countChar("hello", "l"));
-console.log(countChar("hello", "h"));
+//console.log(countChar("hello", "l"));
+//console.log(countChar("hello", "h"));
 
 //write a function that takes in a string and returns the number of vowels in the string
 
@@ -79,4 +79,66 @@ function countVowels(str) {
     return count;
 }
 
-console.log(countVowels("hello"));
+//console.log(countVowels("hello"));
+
+// Given a number n, for each interger i in the range 1 to n, print one value per line as follows:
+// if i is a multiple of both 3 and 5, print "FizzBuzz"
+// if i is a multiple of 3, print "Fizz"
+// if i is a multiple of 5, print "Buzz"
+// if i is not a multiple of 3 or 5, print the value of i
+
+
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// fizzBuzz(5);
+
+
+//console.log(fizzBuzz(15));
+
+// A climber is trying to reach a flag that is some height above the ground. In the attempt to reach the flag, the climber can make any number of jumps up the rock wall where the flag is mounted. Movements can only be made up the wall, and the climber must end at exactly the height of the flag.
+
+// There are two types of jumps:
+// 1. a jump of height 1
+// 2. a jump of bigJump
+
+//Determine the minimum number of jumps required to reach the flag.
+
+
+//Example 
+//flagHeight = 8
+//bigJump = 3
+
+//The climber starts at height 0, takes two jumps of height bigJump, and two of height 1, to react exactly 8 units in 4 jumps.
+
+//jumps has the following values:
+//int flagHeight: an integer representing the height of the flag
+//int bigJump: an integer representing the height of a big jump
+
+//return:
+//an integer representing the minimum number of jumps required to reach the flag
+
+
+function minJumps(flagHeight, bigJump) {
+    let jumps = 0;
+    let height = 0;
+    while (height < flagHeight) {
+        jumps++;
+        height += bigJump;
+        if (height >= flagHeight) {
+            return jumps;
+        }
+        height += 1;
+    }
+}
